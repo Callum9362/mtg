d661cec4582895b47a63785fd762df07f0bcff1e
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-08-27 15:54:03 -0700

    Changed App.vue file

--- mtg-deck/src/App.vue
+++ mtg-deck/src/App.vue
@@ -7,19 +7,7 @@
 
 const mtg = require('mtgsdk')
 
-mtg.card.find(3)
-.then(result => {
-  console.log(result.card.name)
-})
 
-mtg.card.where({ supertypes: 'legendary', subtypes: 'goblin'})
-.then(cards => {
-
-  cards.forEach(card => {
-    console.log(card.name)
-  });
-  
-})
 
 export default {
   name: 'App',
