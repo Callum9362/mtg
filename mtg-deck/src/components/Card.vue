6969b2ddb35e099578e8fc9f0155d4d9dff133c9
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-11-12 17:07:19 -0800

    Finally added the black lotus card to the card component

--- mtg-deck/src/components/Card.vue
+++ mtg-deck/src/components/Card.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>Card Component Here</h1>
+    <div id="whoobe-swr0n" class="pt-4 bg-white w-full md:w-56 justify-center items-center shadow px-6 py-4 flex flex-col">
+	<img v-bind:src="imageURL" alt="img" title="img" class=" h-70 w-40 object-cover" id="whoobe-7jr8o">
+	<h4 class="mt-8 border-b-2" id="whoobe-5f06f">{{ cardName }}</h4>
+</div>
 </template>
 
 <script>
+const mtg = require('mtgsdk')
+
 export default {
-    name: 'Card'
+    name: 'Card',
+    data: function() {
+        return {
+            cardName: mtg.card.find(3).then(result => { this.cardName = result.card.name  }), // Black Lotus
+            imageURL: mtg.card.find(3).then(result => { this.imageURL = result.card.imageUrl  })
+        }
+    }
 }
 </script>
 
