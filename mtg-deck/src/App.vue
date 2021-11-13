ed42ee1cf13aca3651190af63733f00ee2ad0340
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-11-12 17:34:57 -0800

    Added a component to return a list of legendary goblins. Needs styling properly.

--- mtg-deck/src/App.vue
+++ mtg-deck/src/App.vue
@@ -1,15 +1,20 @@
-<template>
-  <Card />
+<template>   
+  <div id="root">
+    <Card />
+    <Goblins/>
+  </div>
 </template>
 
 <script>
 
 import Card from './components/Card'
+import Goblins from './components/Goblins'
 
 export default {
   name: 'App',
   components: {
-    Card
+    Card,
+    Goblins
   }
 }
 </script>
---
+++ mtg-deck/src/components/Goblins.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="Goblins">
+    <div v-for="goblin in goblins" :key="goblin.name" id="whoobe-swr0n" class="pt-4 bg-white w-full md:w-56 justify-center items-center shadow px-6 py-4 flex flex-col">
+	<img v-bind:src="goblin.imageUrl" alt="img" title="img" class=" h-70 w-40 object-cover" id="whoobe-7jr8o">
+	<h4 class="mt-8 border-b-2" id="whoobe-5f06f">{{ goblin.name }}</h4>
+    </div>
+    </div>
+</template>
+
+<script>
+const mtg = require('mtgsdk')
+
+export default {
+    name: 'Goblins',
+    data: function() {
+        return {
+            goblins: mtg.card.where({ supertypes: 'legendary', subtypes: 'goblin' }).then(cards => { this.goblins = cards })
+        }
+    }
+}
+</script>
+
+<style>
+</style>
