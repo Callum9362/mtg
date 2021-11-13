4acc85cbda819d1f5bab6889aa2eaef167941852
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-11-13 11:20:03 -0800

    Added the flex wrap to the goblin cards

--- mtg-deck/src/components/Goblins.vue
+++ mtg-deck/src/components/Goblins.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="Goblins">
+    <div id="Goblins" class="flex flex-wrap">
     <div v-for="goblin in goblins" :key="goblin.name" id="whoobe-swr0n" class="pt-4 bg-white w-full md:w-56 justify-center items-center shadow px-6 py-4 flex flex-col">
 	<img v-bind:src="goblin.imageUrl" alt="img" title="img" class=" h-70 w-40 object-cover" id="whoobe-7jr8o">
 	<h4 class="mt-8 border-b-2" id="whoobe-5f06f">{{ goblin.name }}</h4>
